9813ab3577c840fe0dcb191a158fbd870aaf04a6
215131E <112235122+215131E@users.noreply.github.com>
2023-05-04 04:59:24 -0700

    20230503:usertype-ASH

--- app/src/main/java/com/example/jobinsider/MainActivity.kt
+++ app/src/main/java/com/example/jobinsider/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.jobinsider
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jobinsider.databinding.ActivityLogInBinding
+
+
+import com.example.jobinsider.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivNote.alpha = 0f
+        binding.ivNote.animate().setDuration(1500).alpha(1F).withEndAction{
+            val i = Intent(this,LogIn::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
     }
 }
