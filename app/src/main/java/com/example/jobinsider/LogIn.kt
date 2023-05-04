c699db2a4b3ea4c410a45c1a1bc7a434681d9f1f
215131E <112235122+215131E@users.noreply.github.com>
2023-05-04 05:46:58 -0700

    20230503:usertype-ASH

--- app/src/main/java/com/example/jobinsider/LogIn.kt
+++ app/src/main/java/com/example/jobinsider/LogIn.kt
@@ -3,22 +3,48 @@ package com.example.jobinsider
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import com.example.jobinsider.databinding.ActivityLogInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LogIn : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.loginButton.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, UserType::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
 
-            val intent = Intent(this, UserType::class.java)
-            startActivity(intent)
-
+            binding.signupRedirectText.setOnClickListener {
+                val signupIntent = Intent(this, SignUp::class.java)
+                startActivity(signupIntent)
+            }
         }
     }
+
+
 }
