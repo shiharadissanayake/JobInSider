81aea8f4691eb675061fe07f38f392b5b4f1221c
215131E <112235122+215131E@users.noreply.github.com>
2023-05-04 07:17:19 -0700

    20230503:usertype-ASH

--- app/src/main/java/com/example/jobinsider/JobSeeker.kt
+++ app/src/main/java/com/example/jobinsider/JobSeeker.kt
@@ -1,88 +1,53 @@
 package com.example.jobinsider
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.jobinsider.databinding.ActivityJobSeekerBinding
 import com.google.android.material.navigation.NavigationView
-import com.google.firebase.database.*
 
 class JobSeeker : AppCompatActivity() {
 
-    private lateinit var jsdbref : DatabaseReference
-    private lateinit var jsRecyclerview : RecyclerView
-    private lateinit var jsArrayList : ArrayList<JobData>
+    lateinit var toogle : ActionBarDrawerToggle
 
-
-    private lateinit var binding: ActivityJobSeekerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityJobSeekerBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_job_seeker)
 
-        jsRecyclerview = findViewById(R.id.jobList)
-        jsRecyclerview.layoutManager = LinearLayoutManager(this)
-        jsRecyclerview.setHasFixedSize(true)
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
 
-        jsArrayList = arrayListOf<JobData>()
-        getJobData()
+        toogle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        navView.setNavigationItemSelectedListener {
 
-
-    }
-
-
-
-    private fun getJobData() {
-
-        jsdbref = FirebaseDatabase.getInstance().getReference("Job Vacancies")
-
-        jsdbref.addValueEventListener(object : ValueEventListener {
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-                if (snapshot.exists()) {
-
-                    for (userSnapshot in snapshot.children) {
-
-
-                        val user = userSnapshot.getValue(JobData::class.java)
-                        jsArrayList.add(user!!)
-
-
-                    }
-
-                    jsRecyclerview.adapter = MyAdapter(jsArrayList)
-
-
-
-
-
-                }
-
-
-
-
-
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(applicationContext,"Clicked Home",Toast.LENGTH_SHORT).show()
+                R.id.nav_message -> Toast.makeText(applicationContext,"Clicked Message",Toast.LENGTH_SHORT).show()
+                R.id.nav_sync -> Toast.makeText(applicationContext,"Clicked Sync",Toast.LENGTH_SHORT).show()
+                R.id.nav_trash -> Toast.makeText(applicationContext,"Clicked Delete",Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext,"Clicked Settings",Toast.LENGTH_SHORT).show()
+                R.id.nav_login -> Toast.makeText(applicationContext,"Clicked Login",Toast.LENGTH_SHORT).show()
+                R.id.nav_share -> Toast.makeText(applicationContext,"Clicked Share",Toast.LENGTH_SHORT).show()
+                R.id.nav_rate_us -> Toast.makeText(applicationContext,"Clicked Rate Us",Toast.LENGTH_SHORT).show()
             }
 
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-
-        })
-
+            true
+        }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toogle.onOptionsItemSelected(item)){
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+       }
 }
