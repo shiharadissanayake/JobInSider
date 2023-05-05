3afb2f7187c74eae60c24662ee9a5cfe241aeab2
215131E <112235122+215131E@users.noreply.github.com>
2023-05-04 21:10:23 -0700

    20230505:usertype-ASH

--- app/src/main/java/com/example/jobinsider/JobSeeker.kt
+++ app/src/main/java/com/example/jobinsider/JobSeeker.kt
@@ -9,14 +9,22 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class JobSeeker : AppCompatActivity() {
 
     lateinit var toogle : ActionBarDrawerToggle
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job_seeker)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
 
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
@@ -45,18 +53,56 @@ class JobSeeker : AppCompatActivity() {
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        if(toogle.onOptionsItemSelected(item)){
-
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-       }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//
+//        if(toogle.onOptionsItemSelected(item)){
+//
+//            return true
+//        }
+//        return super.onOptionsItemSelected(item)
+//       }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
         menuInflater.inflate(R.menu.option_menu,menu)
 
         return super.onCreateOptionsMenu(menu)
         }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.delete_user ->{
+                delete_user()
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+        return true
+
+        if(toogle.onOptionsItemSelected(item)){
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun delete_user() {
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            val userRef = database.child("users").child(currentUser.uid)
+            userRef.removeValue()
+            currentUser.delete()
+                .addOnSuccessListener {
+                    Toast.makeText(this,"User deleted Successfully",Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+                .addOnFailureListener{
+                    Toast.makeText(this,"Error deleteing user",Toast.LENGTH_SHORT).show()
+                }
+        }else{
+            Toast.makeText(this,"User not found",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
