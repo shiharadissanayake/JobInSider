3fe000406f6a57f2db1642a43d59dbe179af1add
IT21256196-WE <it21256196@my.sliit.lk>
2023-05-06 23:17:47 +0530

    IT21256196-1

---
+++ app/src/main/java/com/example/jobinsider/reserve_list.kt
@@ -0,0 +1,11 @@
+package com.example.jobinsider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reserve_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserve_list)
+    }
+}
